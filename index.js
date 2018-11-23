8617e82ec13f60b2c594628f1b505d8edd94ce73
Mary Lark <mary@marylark.com>
2018-11-23 18:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function iterativeLog(arr) {
     (element, index, arr) => {console.log(`${index}: ${element}`);}
   );
 }
+
+function iterate(action){
+  var myArr = ["apples", "oranges", "bananas", "avocados"];
+  myArr.forEach(action);
+  return myArr;
+}
+
+function doToArray(arr, action) {
+  arr.forEach(action);
+}
